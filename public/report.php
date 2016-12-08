55776520d4dc8346834d540035f51b9571e4846c
skshultis <shultis.sk@gmail.com>
2016-12-08 09:19:22 -0500

    report.php adds entry to db

--- public/report.php
+++ public/report.php
@@ -2,7 +2,7 @@
 
 <head>
 
-  <title>Aliens Abducted Me - Report an Abduction</title>
+  <title>Aliens Abducted Me!!!</title>
 
 </head>
 
@@ -11,18 +11,54 @@
   <h2>Aliens Abducted Me - Report an Abduction</h2>
 
 <?php
-  $when_it_happened = $_POST['whenithappened'];
-  $how_long = $_POST['howlong'];
-  $aliendescription = $_POST['aliendescription'];
-  $fang_spotted = $_POST['fangspotted'];
-  $email = $_POST['email'];
+$first_name = $_POST['firstname'];
+$last_name = $_POST['lastname'];
+$when_it_happened = $_POST['whenithappened'];
+$how_long = $_POST['howlong'];
+$how_many = $_POST['howmany'];
+$alien_description = $_POST['aliendescription'];
+$what_they_did = $_POST['whattheydid'];
+$fang_spotted = $_POST['fangspotted'];
+$email = $_POST['email'];
+$other = $_POST['other'];
 
-  echo 'Thanks for submitting the form!<br />';
-  echo 'You were abducted '. $when_it_happened;
-  echo ' and were gone for ' . $how_long . '<br />';
-  echo 'Your description: ' . $aliendescription . '<br />';
-  echo 'Was fang there? ' . $fang_spotted . '<br />';
-  echo 'Your email address: ' . $email;
+  // $to = 'shultis@zurka.com';
+  // $subject = 'Aliens Abducted me!!!1!!1!';
+  // $msg = "$name was abducted $whenithappened and was gone for $howlong.\n" .
+  //   "Number of aliens: $howmany.\n" .
+  //   "What they did: $whattheydid.\n" .
+  //   "Fang spotted? $fangspotted.\n" .
+  //   "Other comments: $other";
+  // mail($to, $subject, $msg, 'From:' . $email);
+$dbc = mysqli_connect ('127.0.0.1', 'homestead', 'secret', 'aliendatabase') or die('Ur Moms Pizza');
+
+$query = "INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long, " .
+  "how_many, alien_description, what_they_did, fang_spotted, other, email) " .
+  "VALUES ('$first_name', '$last_name', '$when_it_happened', '$how_long', '$how_many', " .
+  "'$alien_description', '$what_they_did', '$fang_spotted', '$other', '$email')";
+
+  $result = mysqli_query($dbc, $query)
+    or die('Ur Moms Pizza had an error connecting');
+
+  mysqli_close($dbc);
+  // $to = 'shultis@zurka.com';
+  // $subject = 'Aliens Abducted Me - Abduction Report';
+  // $msg = "$name was abducted $whenithappened and was gone for $howlong.\n" .
+  //   "Number of aliens: $howmany\n" .
+  //   "What they did: $whattheydid\n" .
+  //   "Fang spotted: $fangspotted\n" .
+  //   "Other comments: $other";
+  // mail($to, $subject, $msg, 'From:' . $email);
+  //
+  // echo 'Thanks for submitting the form!<br />';
+  // echo 'You were abducted '. $whenithappened;
+  // echo ' and were gone for ' . $howlong . '<br />';
+  // echo 'Number of aliens: ' . $howmany . '<br />';
+  // echo 'Your description: ' . $aliendescription . '<br />';
+  // echo 'The aliens did this: ' . $whattheydid . '<br />';
+  // echo 'Was fang there? ' . $fangspotted . '<br />';
+  // echo 'Other comments: ' . $other . '<br />';
+  // echo 'Your email address: ' . $email;
  ?>
 </body>
 </html>
